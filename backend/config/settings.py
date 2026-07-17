d9ab5e2716b343dc4475f39770c26122fc54fa2c
siddharthg-7 <siddharthgoudgilakathi@gmail.com>
2026-07-17 21:53:37 +0530

    fix(backend): remove DRF throttling to fix 'Too Many Requests' error on proxy deployments

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -198,14 +198,6 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
-    'DEFAULT_THROTTLE_CLASSES': [
-        'rest_framework.throttling.AnonRateThrottle',
-        'rest_framework.throttling.UserRateThrottle'
-    ],
-    'DEFAULT_THROTTLE_RATES': {
-        'anon': '20/min',
-        'user': '100/min'
-    }
 }
 
 SIMPLE_JWT = {
